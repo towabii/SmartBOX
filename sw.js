38c6e6f49ee4913890022adf507c2e57ee6a7f0c
towabii <towa.soko0406@gmail.com>
2025-11-24 13:59:43 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'smartbox-v19';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './smartbox.jpeg',
+  'https://unpkg.com/html5-qrcode',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest',
+  'https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet@latest',
+  'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,1,0'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
