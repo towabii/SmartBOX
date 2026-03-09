63083ab3d6231c9bdcce3d4d3f16e39a8f2a4a90
towabii <towa.soko0406@gmail.com>
2026-03-09 18:05:03 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,11 @@
-const CACHE_NAME = 'smartbox-v19';
-
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/smartbox.jpeg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  return self.clients.claim();
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  // デフォルトでは何もしない
 });
